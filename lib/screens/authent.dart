f432e34d14c605dca8ec6d329b004f0469cd8f96
Vlad <funnytrofi@gmail.com>
2022-08-14 23:45:40 +0600

    add auth screen

---
+++ lib/screens/authent.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScreenAuth extends StatelessWidget {
+  const ScreenAuth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      
+    );
+  }
+}
