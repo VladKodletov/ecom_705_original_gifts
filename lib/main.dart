443a21c78b3351a1e26288f6128594deb91e29e7
Vlad <funnytrofi@gmail.com>
2022-08-16 23:31:58 +0600

    add appBar add theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(
     MaterialApp(
       home: const ScreenAuth(),
+      theme: ThemeData,
     ),
   );
 }
--- lib/screens/authent.dart
+++ lib/screens/authent.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
 
-class ScreenAuth extends StatelessWidget {
+class ScreenAuth extends StatefulWidget {
   const ScreenAuth({Key? key}) : super(key: key);
 
+  @override
+  State<ScreenAuth> createState() => _ScreenAuthState();
+}
+
+class _ScreenAuthState extends State<ScreenAuth> {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              height: deviceSize.height,
-              width: deviceSize.width,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Flexible(
-                      child: Text('705 shop'),
-                    ),
-                  ]),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('705 Shop'),
       ),
     );
   }
