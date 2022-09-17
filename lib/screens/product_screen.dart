57238452052d180ae5173d00ef34249b2351cd74
Vlad <funnytrofi@gmail.com>
2022-09-17 23:50:58 +0600

    edit product page

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -8,26 +8,59 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.green,
-                Color(0xFF0ACF83),
-              ],
-              begin: Alignment.bottomRight,
-              end: Alignment.topLeft,
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.green,
+                  Color(0xFF0ACF83),
+                ],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              ),
             ),
           ),
         ),
-      ),
-      body: const Center(
-        child: Text(
-          'Карточка товара',
-          style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text('price'),
+              const Text('name product'),
+              const Text('3 select button'),
+              SingleChildScrollView(child: Row()),
+              const Text('Review'),
+              const ReviewWidget(),
+              Row(
+                children: const [
+                  Text('Another Product'),
+                  Text('See All'),
+                  SingleChildScrollView(),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Add to cart'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ReviewWidget extends StatelessWidget {
+  const ReviewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(Icons.man),
+        Column(
+          children: const [Text('name'), Text('rate review')],
         ),
-      ),
+        const Text('date review'),
+      ],
     );
   }
 }
